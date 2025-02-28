844541e538d30a358700acbbca1d9513c1d5dfba
Futuredakster <danny.kaikov.m@gmail.com>
2025-02-28 15:06:56 -0700

    Your commit mese

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(bodyParser.json()); // Handle JSON requests
 server.use(bodyParser.urlencoded({ extended: false })); // Handle URL encoded requests
 
 server.post("/", function (req, res) {
-    res.send(req.body.message); // Echo back the message
+    res.send(`"${req.body.message}"`); // Echo back the message
 });
  
 const port = process.env.PORT || 3000;
