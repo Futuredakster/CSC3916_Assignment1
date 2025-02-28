e80bc9c3ac1d40e5d84089e7710086c90522d080
Futuredakster <danny.kaikov.m@gmail.com>
2025-02-28 14:44:56 -0700

    Your commit messge

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var server = express();
 
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
+// Middleware to parse JSON body
+server.use(bodyParser.json()); // Handle JSON requests
+server.use(bodyParser.urlencoded({ extended: false })); // Handle URL encoded requests
 
-server.post("/", function(req,res){
-    console.log("I got" + req.body.message);
-    res.send(req.body.message);
-})
+server.post("/", function (req, res) {
+    console.log("I got: " + req.body.message);
+    res.send(req.body.message); // Echo back the message
+});
 
-server.listen(3000,function(){
-    console.log("we are online");
-})
+server.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
